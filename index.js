d57591a68704a3fb401196fa0246beb34154dbf4
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-04-16 05:37:46 +0300

    fix: all tests passed

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+// multer
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Extract file information from the request
+  const { originalname, mimetype, size } = req.file;
 
+  // Send the extracted information as a response
+  res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
